5e276938493bf90dac7ea9a4ba064d2c2d184ef9
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2020-12-22 22:33:39 -0300

    finalizo ejercicio gatites de fetch

--- clase-85/ejercicio2-gatites.js
+++ clase-85/ejercicio2-gatites.js
@@ -8,25 +8,61 @@ const btn = document.getElementById('btn');
 const urlBase = 'https://api.thecatapi.com/v1/images/search';
 //const apiKey = '9c2da6c3-dd40-4c80-a6d8-dcc3eb2bad3e';
 
-const mostrarImagen = () => {
+const mostrarImagen = (data) => {
     let img = document.createElement("img");
     img.className = "img";
-    img.setAttribute("src", urlBase);
-    document.body.appendChild(img);
-    document.body.innerHTML = img;
+    img.setAttribute("src", `${data}`);
+    img.setAttribute('width', '200px');
+    const contenedor = document.getElementById('contenedor');
+    contenedor.appendChild(img);
+
 }
 
-btn.addEventListener('click', () => {
-    fetch(`${urlBase}`)
+const obtenerGatite = () => {
+    fetch(urlBase)
+
         .then(response => {
             console.log(response)
             return response.json();
         })
         .then(data => {
-            mostrarImagen(data)
+            for (let props in data) {
+                const mamaGatite = data[props];
+                for (let prop in mamaGatite) {
+
+                    if (prop === 'url') {
+                        const gatite = mamaGatite[prop];
+                        console.log(gatite)
+                        mostrarImagen(gatite);
+                    }
+                }
+            }
         })
         .catch(error => {
             console.log(error);
         })
+    fetch(urlBase)
 
-})
+        .then(response => {
+            console.log(response)
+            return response.json();
+        })
+        .then(data => {
+            for (let props in data) {
+                const mamaGatite = data[props];
+                for (let prop in mamaGatite) {
+
+                    if (prop === 'url') {
+                        const gatite = mamaGatite[prop];
+                        console.log(gatite)
+                        mostrarImagen(gatite);
+                    }
+                }
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+btn.addEventListener('click', obtenerGatite);
